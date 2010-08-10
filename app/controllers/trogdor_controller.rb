81ec4165b992fe992a4eed15fe44bae23b09c47e
Brody Berg <brodyberg@gmail.com>
2010-08-10 02:33:17 -0700

    Controller enqueuing code

--- app/controllers/trogdor_controller.rb
+++ app/controllers/trogdor_controller.rb
@@ -1,7 +1,8 @@
 class TrogdorController < ApplicationController
 
   def burninate
-    
+    Resque.enqueue(Trogdor, params[:target])
+    render :text => "Telling Trogdor to burninate #{params[:target]}."
   end
 
 end
